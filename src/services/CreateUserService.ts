cfee4c782fd52556bedabcb749395d23ef25d7ba
Jose Victor Persset <victor.persset@gmail.com>
2021-09-11 22:00:51 -0300

    Create user(Repository,Entity,Service for User Creating and Controller

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,42 @@
+import { getCustomRepository } from "typeorm";
+import { UserRepository } from "../repositories/UserRepository";
+import { hash } from "bcryptjs";
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  password: string;
+  admin?: boolean;
+}
+
+class CreateUserService {
+  async execute({ name, email, password, admin = false }: IUserRequest) {
+    const userRepository = getCustomRepository(UserRepository);
+
+    if (!email) {
+      throw new Error("Email incorreto");
+    }
+    const userAlreadyExists = await userRepository.findOne({
+      email,
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("Usuário já existente");
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    const user = userRepository.create({
+      name,
+      email,
+      password: passwordHash,
+      admin,
+    });
+
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export { CreateUserService };
